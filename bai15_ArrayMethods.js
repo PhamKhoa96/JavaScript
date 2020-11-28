bd7868a90def5104ddff76ec8dd19a46ef3a9870
= <=>
2020-11-29 02:08:43 +0700

    commit to repo

---
+++ bai15_ArrayMethods.js
@@ -0,0 +1,23 @@
+/**
+ * a.concat(b)
+ * a.push(b)
+ * a.pop()
+ * a.shift()
+ * a.unshift()
+ * 
+ * tự đọc trên MDN (Mozilla Developer Network)
+ * google keyword: array method
+ * a.slice
+ * a.splice
+ */
+
+ var a = [1, 2, 3];
+ var b = [10, 20];
+ var c = b.concat(a);
+ console.log(c);
+
+ a.push(5);
+ console.log(a);
+
+ a.pop();
+ console.log(a);
